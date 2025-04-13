a148f79aa4b9999c3a4e6269002f7d5a4a9b847e
Adam <bgisgone@gmail.com>
2025-04-13 19:07:56 +0800

    add adam02

--- sourceTree/ViewController.swift
+++ sourceTree/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("hello world")
         print("hello world2")
+        
+        
+        print("hello adam")
     }
 
 
